9ee7bd29350ccf1c614ffaf96fe6f143f1668df8
Jimmy Ko <ko.jimmy09@gmail.com>
2020-12-29 16:28:27 -0500

    completed practice sprint

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "../public/style.css";
+import animalFacts from './data.jsx';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App animalFacts={animalFacts}/>, document.getElementById("app"));
